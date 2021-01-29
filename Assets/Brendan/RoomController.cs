6e3e1bea2bc539748204e83f4a95d7d61430ec2b
asd <salieri@localhost.localdomain>
2021-01-28 22:58:36 -0500

    push new room + remove spf

--- Assets/Brendan/RoomController.cs
+++ Assets/Brendan/RoomController.cs
@@ -6,6 +6,7 @@ public class RoomController : MonoBehaviour
     public GameObject _tileMap;
     public GameObject _scene;
     public GameObject _nextMap;
+    private GameObject _currentMap;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +14,32 @@ public class RoomController : MonoBehaviour
         if (_LoadedRooms.Count > 0){
             var current_level = this.pickRandomRoom();
             _scene.GetComponent<SceneHandler>().tileMap = current_level;
-            GameObject.Instantiate(_scene.GetComponent<SceneHandler>().tileMap);
+            _currentMap = GameObject.Instantiate(_scene.GetComponent<SceneHandler>().tileMap);
         }
     }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void setNextLevel(){
+        var new_level = this.pickRandomRoom();
+        while (new_level == _scene.GetComponent<SceneHandler>().tileMap){
+            new_level = this.pickRandomRoom();
+        }
+        
+        // Set the transform of the next level to the right a bunch
+        // new_level.GetComponent<Transform>
+    }
+    public void setLevel(){
+        var new_level = this.pickRandomRoom();
+        while (new_level == _scene.GetComponent<SceneHandler>().tileMap){
+            new_level = this.pickRandomRoom();
+        }
+        //_currentMap.GetComponent<SceneHandler>().tileMap
+        Destroy(_currentMap);
+        _scene.GetComponent<SceneHandler>().tileMap = new_level;
+        _currentMap = GameObject.Instantiate(_scene.GetComponent<SceneHandler>().tileMap);
     }
     public List<GameObject> _LoadedRooms;
     GameObject pickRandomRoom(){
