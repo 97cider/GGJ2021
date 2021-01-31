122d7a64fe3e5c25d24ebddf3a4f13c636acc876
Brendan McFadden <bmm349@drexel.edu>
2021-01-31 05:30:20 -0500

    ui skeleton almost done

--- Assets/Brendan/SceneManagemer.cs
+++ Assets/Brendan/SceneManagemer.cs
@@ -5,7 +5,22 @@ using UnityEngine.SceneManagement;
 public class SceneManagemer : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public GameObject creditsPanel;
+
+    public GameObject controlsPanel;
+
+    public void enableCreditsPanel(){
+        controlsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+    public void enableControlsPanel(){
+        creditsPanel.SetActive(false);
+        controlsPanel.SetActive(true);
+    }
+    public void closeAll(){
+        creditsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+    }
     public void moveToPlayScene(){
         SceneManager.LoadScene("MainScene");
     }
@@ -13,6 +28,9 @@ public class SceneManagemer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            closeAll();
+        }
     }
 }
